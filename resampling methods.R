418f062a8c362f49fae7ded9dc10d9dc8329304a
Cbella03 <cbella03@dcs.bbk.ac.uk>
2019-04-19 14:06:25 +0200

    bagging and random forests

--- resampling methods.R
+++ resampling methods.R
@@ -140,9 +140,78 @@ mean(tree.titanic3.pred != survived.test)
 table(tree.titanic3.pred, survived.test)
 
 
-#pruning the tree to reduce variance
+#pruning the tree to reduce variance and to determine the optimal level of tree complexity
+
+set.seed(3)
+cv.titanic3 <- cv.tree(tree.titanic3.train, FUN=prune.misclass)
+print(cv.titanic3)
+
+#according to the results the best tree has 8 or 4 leaves. Since the tree has already 8 leaves, there is no need to prune.
+#However, we can prune it to 4 leaves and calculate the new test error rate.
+
+prune.titanic3 <- prune.misclass(tree.titanic3.train, best=4)
+plot(prune.titanic3)
+text(prune.titanic3, pretty=0)
 
 
+tree.prune.titanic3.pred <- predict(prune.titanic3, titanic3.test, type="class")
+mean(tree.prune.titanic3.pred!=survived.test)
+
+#the error rate is the same as the tree with 8 leaves. However, considering the interpretability, the tree with 4 leaves is better.
+
+#Bagging and Random Forests
+#this analysis will be carried out on an updated version of the titanic dataset. The dataset is available in the Github repository.
+#the dataset needs to be cleaned and transformed (categorical variables)
+library(dplyr) 
+titanic3 <- read.csv("C:/Users/carlo/Desktop/DataAnalysisWithR/titanic3.csv") 
+titanic3 <- select(titanic3,-name, -ticket, -boat, -body, -home.dest, -cabin) %>% mutate(embarked = factor(embarked), sex = factor(sex), pclass=factor(pclass), survived01 = as.factor(titanic3$survived)) 
+summary(titanic3)
+
+nrow(titanic3)
+titanic3 <- na.omit(titanic3)
+nrow(titanic3)
+
+library(randomForest)
+
+#create a training set and a test set
+
+set.seed(8)
+train <- sample(1:nrow(titanic3), nrow(titanic3)/2)
+test <- titanic3[-train,]
+
+survived.test <- titanic3$survived[-train]
+survived01.test <- titanic3$survived01[-train]
+
+
+#the training set can be used to build a bagged model for: y: survived, x: all the features other than survived and survived01
+
+set.seed(6)
+bag.titanic <- randomForest(survived ~ .-survived01, data = titanic3, subset = train, mtry=7, importance=TRUE)
+print(bag.titanic)
+
+bag.pred <- predict(bag.titanic, newdata=test)
+bag.pred.class <- ifelse(bag.pred <= 0.5, "0", "1")
+mean(bag.pred.class!=survived.test) # this is the mean error rate on the test dataset
+
+#a bagged model can be built also on: y: survived01, x: all the features other than survived01 and survived
+
+bag.titanic01 <- randomForest (survived01 ~.-survived, data=titanic3, subset=train, mtry=7, importance=TRUE)
+print(bag.titanic01)
+bag.pred01 <- predict(bag.titanic01, newdata=test, type="class")
+print(mean(bag.pred01!=survived.test))
+
+
+#we check the variable importance for the two previous models
+
+importance(bag.titanic)
+varImpPlot(bag.titanic)
+
+importance(bag.titanic01)
+varImpPlot(bag.titanic01)
+
+barplot(sort(importance(bag.titanic)[,1], decreasing=TRUE), xlab="Relative Importance", col="red", horiz = TRUE, las=1)
+
+#as we can see from the %IncMSE, the importance of both models coincide.
 
 
 
