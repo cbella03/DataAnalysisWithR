a9f0ef7d9ca285619174d4072f57e668ad68c646
Cbella03 <cbella03@dcs.bbk.ac.uk>
2019-04-19 11:26:15 +0200

    decision trees

--- resampling methods.R
+++ resampling methods.R
@@ -103,6 +103,58 @@ for (i in 1:10){
 }
 
 
+#Decision Trees
+#The used dataset (from GitHub) contains some data regarding the features of the passengers of Titanic.
+# I want to use the decision tree technique to predict whether some feature affected the probability of persons to survive.
+
+library(readr)
+library(dplyr)
+
+titanic3 <- "https://goo.gl/At238b" %>% read_csv %>% select(survived, embarked, sex, sibsp, parch, fare) %>% mutate(embarked = factor(embarked), sex = factor(sex))
+
+titanic3$survived <- as.factor(titanic3$survived) #the survived variable is transformed in a categorical variable
+
+#let's fit a classification tree
+
+
+tree.titanic3 <- tree(survived ~ embarked+sex+sibsp+parch+fare, titanic3)
+summary(tree.titanic3)
+plot(tree.titanic3)
+text(tree.titanic3, pretty=0)
+
+# to evaluate the performance of the model I build a tree using a training set and I estimate its error rate on the test data
+
+set.seed(2)
+train <- sample(1:nrow(titanic3), nrow(titanic3)/2)
+titanic3.test <- titanic3[-train,]
+survived.test <- titanic3$survived[-train]
+
+tree.titanic3.train <- tree(survived ~ embarked+sex+sibsp+parch+fare, titanic3, subset=train)
+plot(tree.titanic3.train)
+text(tree.titanic3.train, pretty=0)
+
+tree.titanic3.pred <- predict(tree.titanic3.train, titanic3.test, type="class")
+mean(tree.titanic3.pred != survived.test)
+
+#check:
+table(tree.titanic3.pred, survived.test)
+
+
+#pruning the tree to reduce variance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
