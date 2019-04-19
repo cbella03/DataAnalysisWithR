1904fc1eb3f8ee9b2805a4baed57b47e6e1c0b11
Carlo Bellati <cbella03@dcs.bbk.ac.uk>
2019-04-19 14:42:50 +0200

    Update bagging and r forest.R

--- bagging and r forest.R
+++ bagging and r forest.R
@@ -53,4 +53,30 @@ barplot(sort(importance(bag.titanic)[,1], decreasing=TRUE), xlab="Relative Impor
 #as we can see from the %IncMSE, the importance of both models coincide.
 
 
+x_test <- test[, -c(2,9)] #indicates the test set of all predictors without survived and survived01
+
+bag.titanic01 = randomForest(survived01 ~ .-survived, data=titanic3, subset=train, importance=TRUE, xtest=x_test, ytest=survived01.test, mtry=7, ntree=200)
+# we have inserted the test set when building the model
+
+plot(1:200, bag.titanic01$test$err.rate[,1], type="l", xlab="Number of Bootstrap Data Sets", ylab="Test Error Rate", ylim=c(0.17, 0.30), xlim=c(0,205))
+abline(h=bag.titanic01$test$err.rate[1,1], lty=2, col="red")
+
+yhat.ter.ave <-rep(0,200)# a vector for Test Error Rate using averaging
+for(j in 1:200){
+  set.seed(6)
+  bag.titanic <-randomForest(survived ~ .-survived01,data=titanic3,subset = train,mtry=7,importance = TRUE,ntree=j)
+  bag.pred <-predict(bag.titanic, newdata = test)
+  bag.pred.class <-ifelse(bag.pred<=0.5, "0", "1")
+  yhat.ter.ave[j] <-mean(bag.pred.class!=survived.test)
+}
+
+lines(yhat.ter.ave,col="blue")
+legend("topright",c("single tree","majority vote","averaging prob"),lty=c(2,1,1),col=c("red","black","blue"))
+
+#the plot shows 3 lines:
+#red dashed line: the test error rate of a single tree
+#black curve: the test error rates for majority vote
+#blue curve: the test error rates for averaging the probabilities
+
+
 
